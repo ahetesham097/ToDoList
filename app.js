52ffb17b784d58b65629fb0916eb4233af076a2a
ahetesham097 <105157560+ahetesham097@users.noreply.github.com>
2022-11-18 01:21:07 +0530

    Passing data from webpage to server

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+
+const app = express();
+
+var item = "";
+
+
+app.set("view engine","ejs")
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    var today = new Date();
+    var options = {
+        weekday:"long",
+        day: "numeric",
+        month:"long"
+    }
+    var day = today.toLocaleDateString("en-US",options);
+    res.render("days",{kindsOfDay:day, newListItem:item})
+})
+
+app.post("/", function(req,res){
+
+   var item = req.body.newItem; 
+   res.redirect("/")
+   
+}) 
+
+app.listen(3000, function(){
+    console.log("server Started");
+})
