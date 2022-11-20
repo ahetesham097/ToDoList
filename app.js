1e7d752bb68113becfb8745b88b0e687b0cef273
ahetesham097 <105157560+ahetesham097@users.noreply.github.com>
2022-11-20 23:53:31 +0530

    adding header & footer layouts

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(express.static("public"))
 
 var items = ["Buy Food","Cook Food"];
+var newWorkList = [];
 
 
 
 app.set("view engine","ejs")
+
 
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -22,13 +24,37 @@ app.get("/", function(req, res){
         month:"long"
     }
     var day = today.toLocaleDateString("en-US",options);
-    res.render("days",{kindsOfDay:day, newListItems:items});
+    res.render("days",{listTitle:day, newListItems:items});
 })
 
 app.post("/", function(req,res){
-var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    var item = req.body.newItem;
+
+    if(req.body.list ==="Work"){
+        newWorkList.push(item);
+        res.redirect("/work") 
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
+    
+    
+})
+
+app.get("/work",function(req,res){
+    res.render("days",{listTitle:"Work of the Day",newListItems:newWorkList});
+})
+
+app.post("/work", function(req,res){
+    newWorkList.push(item);
+    res.redirect("/work")
+})
+
+app.get("/about",function(req,res){
+    res.render("about");
 })
 
 app.listen(3000, function(){
